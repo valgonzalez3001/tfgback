4453397fa1e852669bf1fba3eb0fde1d7748c5cb
ymarting <ys.martin@upm.es>
2024-04-03 15:43:50 +0200

    Initial commit

---
+++ src/main/java/es/upm/dit/isst/tfgapi/TFGApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.tfgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TFGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TFGApplication.class, args);
+	}
+
+}
